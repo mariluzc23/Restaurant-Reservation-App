4e08cc803cb47c230f47ad7715030a414a66f9e4
Mariluz <mariluz.colon23@gmail.com>
2022-08-23 00:58:37 -0400

    updated

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,8 +5,8 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL ="https://app-reservation-backend.herokuapp.com"
-   //process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
+const API_BASE_URL =
+   process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
   
 
