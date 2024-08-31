d40ba27db602ec30938279679a6cd8d3c033b638
Mariluz <mariluz.colon23@gmail.com>
2024-08-31 00:18:22 -0400

    updating some inconsistent styling with sidebar

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -9,16 +9,17 @@ import { Link } from "react-router-dom";
 function Menu() {
   return (
     <nav className="navbar navbar-dark align-items-start p-0">
-      <div className="container-fluid d-flex flex-column p-0">
+      <div className="container-fluid d-flex flex-column p-0 vh-100">
         <Link
           className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
           to="/"
         >
-          <div className="sidebar-brand-text mx-3">
+          <div className="sidebar-brand-text mx-3 flex-grow-1 d-flex flex-column flex-nowrap">
             <span>Gourmet Haven</span>
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
+        <div className="flex-grow-1 d-flex flex-column">
         <ul className="nav navbar-nav text-light" id="accordionSidebar">
           <li className="nav-item">
             <Link className="nav-link" to="/dashboard">
@@ -45,6 +46,7 @@ function Menu() {
             </Link>
           </li>
         </ul>
+      </div>  
       </div>
     </nav>
   );
