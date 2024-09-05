3bcba8c728403164fa54beb60a32a82a939589ec
Mariluz <mariluz.colon23@gmail.com>
2024-09-05 12:45:20 -0400

    mobile responsiveness

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -1,10 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-/**
- * Defines the menu for this application.
- *
- * @returns {JSX.Element}
- */
 
 function Menu() {
   return (
@@ -14,39 +9,39 @@ function Menu() {
           className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
           to="/"
         >
-          <div className="sidebar-brand-text mx-3 flex-grow-1 d-flex flex-column flex-nowrap">
+          <div className="sidebar-brand-text mx-3 flex-grow-1">
             <span>Gourmet Haven</span>
           </div>
         </Link>
-        <hr className="sidebar-divider my-0" />
+        <hr className="sidebar-divider my-0 d-none d-md-block" />
         <div className="flex-grow-1 d-flex flex-column">
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
-              <span className="oi oi-dashboard" />
-              &nbsp;Dashboard
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">
-              <span className="oi oi-magnifying-glass" />
-              &nbsp;Search
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/reservations/new">
-              <span className="oi oi-plus" />
-              &nbsp;New Reservation
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/tables/new">
-              <span className="oi oi-layers" />
-              &nbsp;New Table
-            </Link>
-          </li>
-        </ul>
-      </div>  
+          <ul className="nav navbar-nav text-light" id="accordionSidebar">
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">
+                <span className="oi oi-dashboard" />
+                &nbsp;Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/search">
+                <span className="oi oi-magnifying-glass" />
+                &nbsp;Search
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/reservations/new">
+                <span className="oi oi-plus" />
+                &nbsp;New Reservation
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/tables/new">
+                <span className="oi oi-layers" />
+                &nbsp;New Table
+              </Link>
+            </li>
+          </ul>
+        </div>  
       </div>
     </nav>
   );
